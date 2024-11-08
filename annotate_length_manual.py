16962feb75c510b1c593bfdc6e622b2526e6a88b
koenw <koenwentinck@gmail.com>
2024-11-08 15:49:24 +0100

    spotmax

--- annotate_length_manual.py
+++ annotate_length_manual.py
@@ -1,17 +1,20 @@
 import napari 
 import os
- 
+import csv
+
 import h5py as h
 import numpy as np
 import pandas as pd
 
-dir = r"X:\Kleele\Nov_2024\02-Analysis\241108_RPE1_drug_treatments_screen\Ilastik_segmentation"
-save_dir = r"X:\Kleele\Nov_2024\02-Analysis\241108_RPE1_drug_treatments_screen\manual_lengths"
+
+
+dir = r"Z:\Kleele\Nov_2024\02-Analysis\241108_RPE1_drug_treatments_screen\Ilastik_segmentation"
+save_dir = r"Z:\Kleele\Nov_2024\02-Analysis\241108_RPE1_drug_treatments_screen\manual_lengths"
 
 already_list = os.listdir(save_dir)
 
 for file in os.listdir(dir):
-    if file.endswith('_decon.tif'):
+    if file.endswith('_decon_mito.h5'):
         basename = file.split('.')[0]
         # check that we did not already do this one
         if not basename + '_mito_lengths.csv' in already_list:
@@ -24,7 +27,11 @@ for file in os.listdir(dir):
             input('press enter to save ')
 
             lengths = viewer.layers['mito_lengths'].data
-            df = pd.DataFrame(lengths)
-            df.to_csv(os.path.join(save_dir,basename + '_mito_lengths.csv')) 
+            
+            # save as rows to a csv
+            file = open(os.path.join(save_dir,basename + '_mito_lengths.csv'), 'w')
+            a = csv.writer(file)
+            a.writerows(lengths)
+            file.close()
 
             viewer.close()
